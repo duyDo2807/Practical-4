54beba174c81c8b7d2d73e8e3fefdc44d615ba06
duyDo2807 <a1881078@adelaide.edu.au>
2023-09-01 17:31:17 +0930

    Practical 4

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -8,26 +8,25 @@
 
 int main() {
   ParkingLot parkingLot(10);
-  int count = 0;
-  while (count < 10) {
+  while (parkingLot.getCount() < 10) {
     std::string vehicle;
     std::cout << "Enter vehicle type:";
-    std::cin >> vehicle;
+    std::getline(std::cin, vehicle);
     for (char& c : vehicle) {
       c = std::tolower(c);
     }
 
-    Vehicle* newVehicle;
+    Vehicle* newVehicle = nullptr;
     if (vehicle == "car") {
       newVehicle = new Car(parkingLot.getCount() + 1);
     } else if (vehicle == "bus") {
       newVehicle = new Bus(parkingLot.getCount() + 1);
     } else if (vehicle == "motorbike") {
       newVehicle = new Motorbike(parkingLot.getCount() + 1);
+    } else if (vehicle.empty()) {
+      newVehicle = nullptr;
     } else {
-      std::cout
-          << "Invalid vehicle type. Please enter 'car', 'bus', or 'motorbike'."
-          << std::endl;
+      std::cout << "Invalid vehicle type." << std::endl;
       continue;
     }
 
@@ -39,10 +38,9 @@ int main() {
       std::cout << "No vehicle has been parked for ID: "
                 << parkingLot.getCount() << std::endl;
     }
-    count++;
   }
 
-  if (count == 10) {
+  if (parkingLot.getCount() == 10) {
     std::cout << "The lot is full" << std::endl;
   }
 
